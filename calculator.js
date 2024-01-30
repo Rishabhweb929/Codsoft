0b3cd1483c3977c70e9ce5c36d5f03c5c0a318a7
Rishabhweb929 <155287744+Rishabhweb929@users.noreply.github.com>
2024-01-30 19:55:59 +0530

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+<script>
+        function appendToScreen(value) {
+            forms.screen.value += value;
+        }
+
+        function clearScreen() {
+            forms.screen.value = '';
+        }
+
+        function calculateResult() {
+            forms.screen.value = eval(forms.screen.value);
+        }
